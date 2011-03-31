dca46013c2a20e285b075f49bbe264a0fe9dec48
juanza <>
2011-03-31 20:15:25 +0000

    Fixed error in which build was uploaded

--- build/src/com/zorba-xquery/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/juanza/Zorba/modules/excel/src/com/zorba-xquery
-# Build directory: C:/Users/juanza/Zorba/modules/excel/build/src/com/zorba-xquery
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(www)
